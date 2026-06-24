430d45a8c6a4c32d092af9a7efff1c9956e80646
Erick Junior Cantillo Hernandez <erickcantilloh@gmail.com>
2026-06-23 19:30:17 -0500

    feat: se agrego la interactividad en script.js

---
+++ README.md
@@ -0,0 +1,3 @@
+# MiPerfilADSO
+
+MiPerfilADSO es una tarjeta de presentación personal interactiva: una página de una sola vista que te presenta como aprendiz del programa ADSO
---
+++ script.js
@@ -0,0 +1,8 @@
+const boton = document.getElementById("btnModo");
+
+boton.addEventListener("click", () => {
+  document.body.classList.toggle("modo-oscuro");
+});
+
+const anioActual = new Date().getFullYear();
+document.getElementById("anio").textContent = anioActual;
